c6e6cd6dec18271cb62020e057c97da02c4c7195
Rehan <rehanalire52@gmail.com>
2024-03-26 15:47:58 +0530

    models update for chat app

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true,
+        default: 'https://storage.googleapis.com/sticker-prod/RhxqmL4IFuxXNHn3VJco/0.png'
+    }
+},
+{
+    timestamps: true,
+}
+)
+
+const userModel = mongoose.model('userModel', userSchema);
+export default userModel;
